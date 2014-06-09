1aa9c59cebde5b83731e0621e9e4567748a5948d
velikolepnii <k-karuna>
2014-06-09 21:56:53 +0400

    kostya: models added

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=200)
+    addres = models.CharField(max_length=200)
+    phone = models.CharField(max_length=50)
+    description = models.CharField(max_length=256)
+
+class News(models.Model):
+    organization_id = models.ForeignKey(Organization)
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    description = models.CharField(max_length=256)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    organization_id = models.ForeignKey(Organization)
+    privilegies = models.CharField(max_length=200)
